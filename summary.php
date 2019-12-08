eb1027f859deb4836ba2684d6f8cbad5bacfdb18
kbogdanski <kamil004@op.pl>
2019-12-08 17:22:27 +0100

    obsluga przypadku gdy w pliku nie ma danych za wybrane na formularzu lata

--- summary.php
+++ summary.php
@@ -66,30 +66,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $bilans = new Bilans();                         // TWORZENIE OBIEKTU KLASY BILANS - WARIANT ZEROWY
             $bilansWariantBranzowy = new Bilans();          // TWORZENIE OBIEKTU KLASY BILANS - WARIANT BRANŻOWY
             $bilansWariantSredniejDynamiki = new Bilans();  // TWORZENIE OBIEKTU KLASY BILANS - WARIANT ŚREDNIEJ DYNAMIKI
+
+            /* Ladowanie danych do obiektów */
+            $bilans->loadDataForBilansObject($file, $yearsTable, $_POST);
+            $bilans->calculateOthersData(0);
+
+            $bilansWariantBranzowy->loadDataForBilansObject($file, $yearsTable, $_POST);
+            $bilansWariantBranzowy->calculateOthersData(1);
+
+            $bilansWariantSredniejDynamiki->loadDataForBilansObject($file, $yearsTable, $_POST);
+            $bilansWariantSredniejDynamiki->calculateOthersData(2);
+
+            $wartoscDCF = DCF::calculateDCFvalue($bilans->getSzacunekWartosciKapitaluWlasnegoSuma(),
+                $bilansWariantBranzowy->getSzacunekWartosciKapitaluWlasnegoSuma(),
+                $bilansWariantSredniejDynamiki->getSzacunekWartosciKapitaluWlasnegoSuma());
+
+            $yearsTable2 = Wskaznik::CreateBilansTabelYear($file, $rok);
+            $wskaznik = Wskaznik::CreateWskaznik($file, $yearsTable2);
+            $wskaznik->calculateOthersData();
+
+            $_SESSION['bilans'] = serialize($bilans);
+            $_SESSION['bilansWariantBranzowy'] = serialize($bilansWariantBranzowy);
+            $_SESSION['bilansWariantSredniejDynamiki'] = serialize($bilansWariantSredniejDynamiki);
+            $_SESSION['wartoscDCF'] = serialize($wartoscDCF);
+            $_SESSION['wskaznik'] = serialize($wskaznik);
         }
-        /* Ladowanie danych do obiektów */
-        $bilans->loadDataForBilansObject($file, $yearsTable, $_POST);
-        $bilans->calculateOthersData(0);
-
-        $bilansWariantBranzowy->loadDataForBilansObject($file, $yearsTable, $_POST);
-        $bilansWariantBranzowy->calculateOthersData(1);
-
-        $bilansWariantSredniejDynamiki->loadDataForBilansObject($file, $yearsTable, $_POST);
-        $bilansWariantSredniejDynamiki->calculateOthersData(2);
-
-        $wartoscDCF = DCF::calculateDCFvalue($bilans->getSzacunekWartosciKapitaluWlasnegoSuma(),
-            $bilansWariantBranzowy->getSzacunekWartosciKapitaluWlasnegoSuma(),
-            $bilansWariantSredniejDynamiki->getSzacunekWartosciKapitaluWlasnegoSuma());
-
-        $yearsTable2 = Wskaznik::CreateBilansTabelYear($file, $rok);
-        $wskaznik = Wskaznik::CreateWskaznik($file, $yearsTable2);
-        $wskaznik->calculateOthersData();
-
-        $_SESSION['bilans'] = serialize($bilans);
-        $_SESSION['bilansWariantBranzowy'] = serialize($bilansWariantBranzowy);
-        $_SESSION['bilansWariantSredniejDynamiki'] = serialize($bilansWariantSredniejDynamiki);
-        $_SESSION['wartoscDCF'] = serialize($wartoscDCF);
-        $_SESSION['wskaznik'] = serialize($wskaznik);
     }
 
     //var_dump($_POST);
@@ -119,81 +120,91 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-sm-3"></div>
-        <div class="col-sm-6">
-            <form role="form" method="post" action="summary.php">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th>ROK</th>
-                        <th>Amortyzacja</th>
-                        <th>Nowa wartość</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <th scope="row"><?php echo $bilans->getRok0() ?></th>
-                        <td><?php echo number_format($bilans->getAmortyzacja()[0],2,',',' ')." zł" ?></td>
-                        <td><input type="text" class="form-control" value=0.00 name="amortyzacja0"></td>
-                    </tr>
-                    <tr>
-                        <th scope="row"><?php echo $bilans->getRok1() ?></th>
-                        <td><?php echo number_format($bilans->getAmortyzacja()[1],2,',',' ')." zł" ?></td>
-                        <td><input type="text" class="form-control" value=0.00 name="amortyzacja1"></td>
-                    </tr>
-                    <tr>
-                        <th scope="row"><?php echo $bilans->getRok2() ?></th>
-                        <td><?php echo number_format($bilans->getAmortyzacja()[2],2,',',' ')." zł" ?></td>
-                        <td><input type="text" class="form-control" value=0.00 name="amortyzacja2"></td>
-                    </tr>
-                    </tbody>
-                </table>
+    <?php if (isset($bilans)) { ?>
+        <div class="row">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-6">
+                <form role="form" method="post" action="summary.php">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>ROK</th>
+                            <th>Amortyzacja</th>
+                            <th>Nowa wartość</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row"><?php echo $bilans->getRok0() ?></th>
+                            <td><?php echo number_format($bilans->getAmortyzacja()[0],2,',',' ')." zł" ?></td>
+                            <td><input type="text" class="form-control" value=0.00 name="amortyzacja0"></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><?php echo $bilans->getRok1() ?></th>
+                            <td><?php echo number_format($bilans->getAmortyzacja()[1],2,',',' ')." zł" ?></td>
+                            <td><input type="text" class="form-control" value=0.00 name="amortyzacja1"></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"><?php echo $bilans->getRok2() ?></th>
+                            <td><?php echo number_format($bilans->getAmortyzacja()[2],2,',',' ')." zł" ?></td>
+                            <td><input type="text" class="form-control" value=0.00 name="amortyzacja2"></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <?php
+                    if (($bilans->getAmortyzacja()[0]) == 0 || ($bilans->getAmortyzacja()[1]) == 0 || ($bilans->getAmortyzacja()[2]) == 0) {
+                        echo "<div class='alert alert-danger'>Amortyzacja w jedym lub kilku wczytanach latach wynosi 0.
+                                                              Przed wygenerowaniem raportu podaj wartośc Amortyzacji dla lat w których jej wartość wynosi 0.</div>";
+                    } else {
+                        echo "<div class='alert alert-success'>Zaczytane dane zawierają wartość Amortyzacji</div>";
+                    }
+                    ?>
+                    <p><button type="submit" class="btn btn-primary btn-block">Przelicz z nowymi wartościami amortyzacji</button></p>
+                </form>
+            </div>
+            <div class="col-sm-3"></div>
+        </div>
+        <dl class="row">
+            <dt class="col-sm-4">Nazwa firmy:</dt>
+            <dd class="col-sm-8"><?php echo $bilans->getFirma() ?></dd>
+            <dt class="col-sm-4">Lata do bilansu firmy:</dt>
+            <ul class="col-sm-8">
+                <li><?php echo $bilans->getRok0() ?></li>
+                <li><?php echo $bilans->getRok1() ?></li>
+                <li><?php echo $bilans->getRok2() ?></li>
+            </ul>
+            <dt class="col-sm-4">Lata do analizy:</dt>
+            <ul class="col-sm-8">
                 <?php
-                if (($bilans->getAmortyzacja()[0]) == 0 || ($bilans->getAmortyzacja()[1]) == 0 || ($bilans->getAmortyzacja()[2]) == 0) {
-                    echo "<div class='alert alert-danger'>Amortyzacja w jedym lub kilku wczytanach latach wynosi 0.
-                                                          Przed wygenerowaniem raportu podaj wartośc Amortyzacji dla lat w których jej wartość wynosi 0.</div>";
-                } else {
-                    echo "<div class='alert alert-success'>Zaczytane dane zawierają wartość Amortyzacji</div>";
+                foreach($wskaznik->getYearsTabel() as $value) {
+                    echo "<li>$value</li>";
                 }
                 ?>
-                <p><button type="submit" class="btn btn-primary btn-block">Przelicz z nowymi wartościami amortyzacji</button></p>
-            </form>
+            </ul>
+            <dt class="col-sm-4">Wariant zerowy - DCF:</dt>
+            <dd class="col-sm-8"><?php echo number_format($bilans->getSzacunekWartosciKapitaluWlasnegoSuma(),2,',',' ') ?></dd>
+            <dt class="col-sm-4">Wariant branżowy - DCF:</dt>
+            <dd class="col-sm-8"><?php echo number_format($bilansWariantBranzowy->getSzacunekWartosciKapitaluWlasnegoSuma(),2,',',' ') ?></dd>
+            <dt class="col-sm-4">Wariant średniej dynamiki - DCF:</dt>
+            <dd class="col-sm-8"><?php echo number_format($bilansWariantSredniejDynamiki->getSzacunekWartosciKapitaluWlasnegoSuma(),2,',',' ') ?></dd>
+            <dt class="col-sm-4">Wartość szacowana metodą DCF:</dt>
+            <dd class="col-sm-8"><?php echo number_format($wartoscDCF,2,',',' ') ?></dd>
+            <dt class="col-sm-4">Wartość szacowana metodą likwidacyjną:</dt>
+            <dd class="col-sm-8"><?php echo number_format($bilans->getWartoscLikwidacyjna(),2,',',' ') ?></dd>
+        </dl>
+        <div class="row">
+            <div class="col-sm-12">
+                <a class="btn btn-success btn-block" href="generate.php">GENERUJ RAPORT</a>
+            </div>
         </div>
-        <div class="col-sm-3"></div>
-    </div>
-    <dl class="row">
-        <dt class="col-sm-4">Nazwa firmy:</dt>
-        <dd class="col-sm-8"><?php echo $bilans->getFirma() ?></dd>
-        <dt class="col-sm-4">Lata do bilansu firmy:</dt>
-        <ul class="col-sm-8">
-            <li><?php echo $bilans->getRok0() ?></li>
-            <li><?php echo $bilans->getRok1() ?></li>
-            <li><?php echo $bilans->getRok2() ?></li>
-        </ul>
-        <dt class="col-sm-4">Lata do analizy:</dt>
-        <ul class="col-sm-8">
-            <?php
-            foreach($wskaznik->getYearsTabel() as $value) {
-                echo "<li>$value</li>";
-            }
-            ?>
-        </ul>
-        <dt class="col-sm-4">Wariant zerowy - DCF:</dt>
-        <dd class="col-sm-8"><?php echo number_format($bilans->getSzacunekWartosciKapitaluWlasnegoSuma(),2,',',' ') ?></dd>
-        <dt class="col-sm-4">Wariant branżowy - DCF:</dt>
-        <dd class="col-sm-8"><?php echo number_format($bilansWariantBranzowy->getSzacunekWartosciKapitaluWlasnegoSuma(),2,',',' ') ?></dd>
-        <dt class="col-sm-4">Wariant średniej dynamiki - DCF:</dt>
-        <dd class="col-sm-8"><?php echo number_format($bilansWariantSredniejDynamiki->getSzacunekWartosciKapitaluWlasnegoSuma(),2,',',' ') ?></dd>
-        <dt class="col-sm-4">Wartość szacowana metodą DCF:</dt>
-        <dd class="col-sm-8"><?php echo number_format($wartoscDCF,2,',',' ') ?></dd>
-        <dt class="col-sm-4">Wartość szacowana metodą likwidacyjną:</dt>
-        <dd class="col-sm-8"><?php echo number_format($bilans->getWartoscLikwidacyjna(),2,',',' ') ?></dd>
-    </dl>
-    <div class="row">
+    <?php } else { ?>
+        <div class='alert alert-danger'>
+            Wczytany plik nie zawiera danych za wybrane lata <?php echo "(".$rok." - ".($rok-2).")" ?>.
+            Brak możliwości wygenerowania raportu ze względu na brak danych.
+        </div>
         <div class="col-sm-12">
-            <a class="btn btn-success btn-block" href="generate.php">GENERUJ RAPORT</a>
+            <a class="btn btn-primary btn-block" href="index.php">STRONA GŁÓWNA</a>
         </div>
-    </div>
+    <?php } ?>
 </div>
 </body>
